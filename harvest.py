9edcec93087923d45d1a288eedef548c61dbd542
Erin Falcone <erinlfalcone@gmail.com>
2021-09-10 20:05:11 +0000

    Generate dictionary of melons from types.txt

--- harvest.py
+++ harvest.py
@@ -11,28 +11,71 @@ class MelonType(object):
         """Initialize a melon."""
 
         self.pairings = []
-
-        # Fill in the rest
+        self.code = code
+        self.f_harv = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
+        self.name = name
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
 
-        # Fill in the rest
+        self.pairings.append(pairing)
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
 
-        # Fill in the rest
+        self.code = new_code
 
 
 def make_melon_types():
-    """Returns a list of current melon types."""
+    """Returns a list of current melon types (see types.txt for the list)."""
 
     all_melon_types = []
+
+    # loop?
+        # instantiates the class MelonType for each of the above melon types.
+        # After you instantiate a melon, also call the add_pairing method to add necessary pairings to the instance’s pairings attribute.
+        # add each MelonType instance (object) to a list
+
+    # return a list of objects of the class MelonType
+
 
     # Fill in the rest
 
     return all_melon_types
+
+
+def help_make_melons(filename):
+    """Create and return a dictionary of current melon types using types.txt as filename."""
+    the_file = open(filename)
+    mydict = {}
+    for line in the_file:
+        print(line)
+        if ':' in line:
+            key, value = line.lower().rstrip().split(': ')
+            print(f"key is {key} and value is {value}")
+            if key == 'name':
+                melon_key = value
+                mydict[melon_key] = {}
+                n_dict = mydict[melon_key]  # refers to nested dictionary
+
+            # adding key-value pairs to nested dictionary (mydict[melon_key] aka n_dict)
+            if key == 'pairings':
+                # for pairings, split by commas
+                pairing_options = value.split(', ')
+                n_dict[key] = pairing_options
+            else:
+                n_dict[key] = value if value not in ('true', 'false') else True if value in ('true') else False
+    the_file.close()
+    return mydict
+
+
+file = 'types.txt'
+melon_dict = help_make_melons(file)
+
+
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
